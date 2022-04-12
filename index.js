7fdfc0364db14722cdeb61dc0b3649f5a796b98c
ddanilooalves <daniloalves0512@gmail.com>
2022-04-11 21:36:07 -0300

    configuração

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 const express = require('express');
+const cors = require('cors');
 const port = 3001;
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+const paletas = [
+  {
+    id: 1,
+    sabor: 'Açaí com Leite Condensado',
+    descricao:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    foto: 'assets/images/acai-com-leite-condensado.png',
+    preco: 10.0,
+  },
+  {
+    id: 2,
+    sabor: 'Banana com Nutella',
+    descricao:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    foto: 'assets/images/banana-com-nutella.png',
+    preco: 10.0,
+  },
+  {
+    id: 3,
+    sabor: 'Chocolate Belga',
+    descricao:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    foto: 'assets/images/chocolate-belga.png',
+    preco: 7.0,
+  },
+];
+
 app.get('/', function (req, res) {
   res.send('Hello World');
+});
+
+app.get('/paletas/find-paletas', (req, res) => {
+  res.send(paletas);
+});
+
+app.get('/paletas/find-paletas/:id', (req, res) => {
+  res.send(paletas.find((paleta) => paleta.id == req.params.id));
 });
 
 app.listen(port, () => {
